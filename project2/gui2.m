fa9fe1fdf73d3ff61a7b0df67618b097dc2827a0
shupin <kntnyn@163.com>
2015-10-21 20:13:44 +0800

    project2
    project2

---
+++ project2/gui2.m
@@ -0,0 +1,227 @@
+function varargout = gui2(varargin)
+% GUI2 MATLAB code for gui2.fig
+%      GUI2, by itself, creates a new GUI2 or raises the existing
+%      singleton*.
+%
+%      H = GUI2 returns the handle to a new GUI2 or the handle to
+%      the existing singleton*.
+%
+%      GUI2('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUI2.M with the given input arguments.
+%
+%      GUI2('Property','Value',...) creates a new GUI2 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before gui2_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to gui2_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help gui2
+
+% Last Modified by GUIDE v2.5 21-Oct-2015 14:50:50
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @gui2_OpeningFcn, ...
+                   'gui_OutputFcn',  @gui2_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before gui2 is made visible.
+function gui2_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to gui2 (see VARARGIN)
+
+% Choose default command line output for gui2
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes gui2 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = gui2_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+trial
+a=importdata('diary');
+b=a(1);
+b=b{1};
+delete diary
+if size(b)==[1 36]
+    set(handles.warning,'string',b);
+end
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+fs=str2double(get(handles.edit1,'String'));
+tips1=strcat('(<',num2str(round(1000/fs)),')');
+set(handles.tip2,'string',tips1);
+setappdata(0,'fs',fs);
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+f=getappdata(0,'fs');
+sr=str2double(get(handles.edit2,'String'));
+tips2=strcat('(<',num2str(round(1000/sr)),')');
+set(handles.tip1,'string',tips2);
+setappdata(0,'sr',sr);
+if f*sr>0
+    set(handles.pushbutton1, 'Enable', 'on');
+    set(handles.Pause_pushbutton, 'Enable', 'on');
+    set(handles.pushbutton3, 'Enable', 'on');
+end
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in Pause_pushbutton.
+function Pause_pushbutton_Callback(hObject, eventdata, handles)
+% hObject    handle to Pause_pushbutton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+text = get(hObject, 'String');
+% If the simulation were running:
+t=getappdata(0,'t1');
+if strcmp(text, 'Pause') == 1
+   set(hObject, 'String', 'Countinue');
+    % pause it:
+  stop(t);
+else
+    % otherwise, "resume" it:
+    set(hObject, 'String', 'Pause');
+    start(t);
+end;
+return;
+
+
+% --- Executes on button press in Continuous.
+function Continuous_Callback(hObject, eventdata, handles)
+% hObject    handle to Continuous (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% Hint: get(hObject,'Value') returns toggle state of Continuous
+if get(handles.Continuous,'Value')==1
+    set(handles.certain_time, 'Enable', 'off');
+    set(handles.edit3, 'Enable', 'off');
+    setappdata(0,'lt',inf);
+else
+    set(handles.certain_time, 'Enable', 'on');
+    set(handles.edit3, 'Enable', 'on');
+end
+
+% --- Executes on button press in certain_time.
+function certain_time_Callback(hObject, eventdata, handles)
+% hObject    handle to certain_time (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% Hint: get(hObject,'Value') returns toggle state of certain_time
+if get(handles.certain_time,'Value')==1
+    set(handles.Continuous, 'Enable', 'off');
+else
+    set(handles.Continuous, 'Enable', 'on');
+end
+
+function edit3_Callback(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit3 as text
+%        str2double(get(hObject,'String')) returns contents of edit3 as a double
+lt=str2double(get(handles.edit3,'String'));
+setappdata(0,'lt',lt);
+
+
+% --- Executes during object creation, after setting all properties.
+function edit3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+t=getappdata(0,'t1');
+stop(t);
+delete(t);
+quit;
